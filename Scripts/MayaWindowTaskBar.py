2d67e392b9c149b80bcdc480c83ef3bef3ae00b9
MirageYM <Opteron150@gmail.com>
2015-09-28 00:08:34 +0900

    Add Support :Floating Attribute Editor, Channel Editor and Tool Setting.

--- Scripts/MayaWindowTaskBar.py
+++ Scripts/MayaWindowTaskBar.py
@@ -1,4 +1,38 @@
-﻿import json
+﻿# -*- coding: utf-8 -*-
+
+'''
+**********************
+MayaWindowTaskBar
+Rev.3
+**********************
+
+License
+Copyright (c) 2015, yasutoshi Mori All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1.Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+2.Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+https://github.com/MirageYM
+
+
+**********************
+Usage:
+
+<<to START>>:
+import MayaWindowTaskBar
+MayaWindowTaskBar.start()
+
+<<to STOP>>:
+MayaWindowTaskBar.stop()
+
+'''
+
+
+import json
 import sys
 import os
 import thread
@@ -15,6 +49,112 @@ from PySide.QtUiTools import *
 import shiboken
 
 
+buttonSz = [ 36, 20 ]
+buttonFontSz = 7
+contextFontSz = 9
+
+#-----------------------------------------------
+# class BtnBase
+#-----------------------------------------------
+class BtnBase( QPushButton ):
+	#-----------------------------------------------
+	def __init__( self, title ):
+		super( BtnBase, self ).__init__( title )
+		
+		sz = QSize( buttonSz[0], buttonSz[1] )
+		self.setFixedSize( sz )
+		self.setStyleSheet('text-align:left; font-size:%dpt;' % buttonFontSz )
+		
+		self.menu = QMenu( self )
+		self.menu.setStyleSheet( 'text-align:left; font-size:%dpt;' % contextFontSz )
+
+		self.clicked.connect( self.onButtonClicked )
+		self.setContextMenuPolicy( Qt.CustomContextMenu )
+		self.customContextMenuRequested.connect( self.onRightButtonClicked )
+		
+	#-----------------------------------------------
+	def getWidgetInstance( self ):
+		return None
+	
+	#-----------------------------------------------
+	def onButtonClicked( self ):
+		pass
+	
+	#-----------------------------------------------
+	def onRightButtonClicked( self ):
+		pass
+
+
+#-----------------------------------------------
+# class BtnWindow
+#-----------------------------------------------
+class BtnWindow( BtnBase ):
+	#-----------------------------------------------
+	def __init__( self, windowName ):
+		title = maya.cmds.window( windowName, query = True, title = True )
+		super( BtnWindow, self ).__init__( title )
+
+		self.menu.addAction( 'minimize', self.onMenuMinClicked )
+		self.menu.addAction( 'restore', self.onMenuResClicked )
+		self.menu.addAction( 'close', self.onMenuCloseClicked )
+		
+		self.windowName = windowName
+
+	#-----------------------------------------------
+	def getWidgetInstance( self ):
+		p = OpenMayaUI.MQtUtil.findWindow( self.windowName )
+		return shiboken.wrapInstance( long( p ), QWidget )
+		
+	#-----------------------------------------------
+	def onMenuMinClicked( self ):
+		self.getWidgetInstance().showMinimized()
+		
+	#-----------------------------------------------
+	def onMenuResClicked( self ):
+		self.getWidgetInstance().showNormal()
+		
+	#-----------------------------------------------
+	def onMenuCloseClicked( self ):
+		maya.cmds.deleteUI( self.windowName )
+		
+	#-----------------------------------------------
+	def onButtonClicked( self ):
+		self.getWidgetInstance().showNormal()
+		maya.cmds.setFocus( self.windowName )
+	
+	#-----------------------------------------------
+	def onRightButtonClicked( self ):
+		self.menu.popup( QCursor.pos() )
+	
+
+#-----------------------------------------------
+# class BtnLayout
+#-----------------------------------------------
+class BtnLayout( BtnBase ):
+	#-----------------------------------------------
+	def __init__( self, layoutName ):
+		p = OpenMayaUI.MQtUtil.findLayout( layoutName )
+		i = shiboken.wrapInstance( long( p ), QWidget )
+		parent = i.parent()
+		
+		super( BtnLayout, self ).__init__( parent.windowTitle() )
+
+		self.windowName = layoutName
+		
+	#-----------------------------------------------
+	def getWidgetInstance( self ):
+		p = OpenMayaUI.MQtUtil.findLayout( self.windowName )
+		return shiboken.wrapInstance( long( p ), QWidget )
+	
+	#-----------------------------------------------
+	def onButtonClicked( self ):
+		self.getWidgetInstance().showNormal()
+		maya.cmds.setFocus( self.windowName )
+
+	
+#-----------------------------------------------
+# class WindowManager
+#-----------------------------------------------
 class WindowManager( object ):
 	selfInst = None
 	
@@ -30,27 +170,25 @@ class WindowManager( object ):
 		self.targetLayout = qtToolBoxLayoutChildren[1].children()[2].children()[1].children()[0]
 
 		self.windowLists = []
+		self.dockLists = []
 		self.buttons = []
 
 		self._mainLoopThread = None
 		self._loop_run = True
 
-		self.buttonSz = [ 36, 20 ]
-		self.buttonFontSz = 7
-		self.contextFontSz = 9
+		self.targetDockWindow = [ 'MayaWindow|MainAttributeEditorLayout', 'MayaWindow|MainToolSettingsLayout', 'MayaWindow|MainChannelsLayersLayout' ]
 
 		self.controlButton = self.buildControlButtons()
-
 
 	#-----------------------------------------------
 	def buildControlButtons( self ):
 		
 		newBtn = QToolButton()
-		sz = QSize( self.buttonSz[0], self.buttonSz[1] )
+		sz = QSize( buttonSz[0], buttonSz[1] )
 		newBtn.setFixedSize( sz )
 		
 		menu = QMenu(newBtn)
-		menu.setStyleSheet( 'text-align:left; font-size:%dpt;' % self.contextFontSz )
+		menu.setStyleSheet( 'text-align:left; font-size:%dpt;' % contextFontSz )
 		menu.addAction( 'minimize all', self.menuMinAllFactory() )
 		menu.addAction( 'restore all', self.menuResAllFactory() )
 		menu.addAction( 'close all', self.menuCloseAllFactory() )
@@ -101,27 +239,18 @@ class WindowManager( object ):
 	def buildWindowButtons( self ):
 
 		for w in self.windowLists:
-			
-			title = maya.cmds.window( w, query = True, title = True )
-			newBtn = QPushButton( title )
-
-			sz = QSize( self.buttonSz[0], self.buttonSz[1] )
-			newBtn.setFixedSize( sz )
-			newBtn.setStyleSheet('text-align:left; font-size:%dpt;' % self.buttonFontSz )
-			
-			menu = QMenu(newBtn)
-			menu.setStyleSheet('text-align:left; font-size:9pt;' )
-			menu.addAction( 'minimize', self.menuMinFactory(w) )
-			menu.addAction( 'restore', self.menuResFactory(w) )
-			menu.addAction( 'close', self.menuCloseFactory(w) )
-			
-			newBtn.clicked.connect( self.onButtonClickedFactory(w) )
-			newBtn.setContextMenuPolicy( Qt.CustomContextMenu )
-			newBtn.customContextMenuRequested.connect( self.onRightButtonClickedFactory( menu ) )
-			
+			newBtn = BtnWindow( w )
 			self.buttons.append( newBtn )
 			self.targetLayout.addWidget( newBtn )
 	
+	#-----------------------------------------------
+	def buildDockLayoutButtons( self ):
+
+		for w in self.dockLists:
+			newBtn = BtnLayout( w )
+			self.buttons.append( newBtn )
+			self.targetLayout.addWidget( newBtn )
+			
 	#-----------------------------------------------
 	def removeWindowButtons( self ):
 		for item in self.buttons:
@@ -131,9 +260,36 @@ class WindowManager( object ):
 		self.buttons = []
 
 	#-----------------------------------------------
+	def isDockLayoutFloating( self, s ):
+		p = OpenMayaUI.MQtUtil.findLayout( s )
+		i = shiboken.wrapInstance( long( p ), QWidget )
+		parent = i.parent()
+		return maya.cmds.dockControl( parent.objectName(), q = True, fl = True )
+
+	#-----------------------------------------------
+	def listFloatDock( self ):
+		r = []
+		for s in self.targetDockWindow:
+			if( self.isDockLayoutFloating( s ) ):
+				r.append( s )
+
+		return r
+
+	#-----------------------------------------------
 	def updateButtons( self ):
 
+		def cmpArray( l, r ):
+			if( len( l ) == len( r ) ):
+				for i in range( len( l ) ):
+					if( l[i] != r[i] ):
+						return False
+				return True
+			else:
+				return False
+
 		wList = maya.cmds.lsUI( windows = True )
+		dList = self.listFloatDock()
+		
 		try:
 			#avoid Main window
 			wList.remove( 'MayaWindow' )
@@ -142,63 +298,15 @@ class WindowManager( object ):
 		except:
 			pass
 
-		rebuild = False
-		if( len( wList ) == len( self.windowLists ) ):
-			for i in range( len( wList ) ):
-				if( wList[i] != self.windowLists[i] ):
-					rebuild = True
-					break
-		else:
-			rebuild = True
-
-		if( not rebuild ):
+		if( cmpArray( wList, self.windowLists ) and cmpArray( dList, self.dockLists ) ):
 			return
 			
 		self.removeWindowButtons()
 		self.windowLists = wList
-		
+		self.dockLists = dList
+
+		self.buildDockLayoutButtons()
 		self.buildWindowButtons()
-
-	#-----------------------------------------------
-	def onButtonClickedFactory( self, w ):
-		def clickedImp():
-			p = OpenMayaUI.MQtUtil.findWindow( w )
-			i = shiboken.wrapInstance( long( p ), QWidget )
-			i.showNormal()
-			maya.cmds.setFocus( w )
-		return clickedImp
-
-	#-----------------------------------------------
-	def menuCloseFactory( self, w ):
-		def imp():
-			maya.cmds.deleteUI( w )
-
-		return imp
-	
-	#-----------------------------------------------
-	def menuMinFactory( self, w ):
-		def imp():
-			p = OpenMayaUI.MQtUtil.findWindow( w )
-			i = shiboken.wrapInstance( long( p ), QWidget )
-			i.showMinimized()
-
-		return imp
-	
-	#-----------------------------------------------
-	def menuResFactory( self, w ):
-		def imp():
-			p = OpenMayaUI.MQtUtil.findWindow( w )
-			i = shiboken.wrapInstance( long( p ), QWidget )
-			i.showNormal()
-
-		return imp
-	
-	#-----------------------------------------------
-	def onRightButtonClickedFactory( self, menu ):
-		def clickedImp():
-			menu.popup(QCursor.pos())
-
-		return clickedImp
 
 	#-----------------------------------------------
 	## startLoop
@@ -239,9 +347,13 @@ class WindowManager( object ):
 		WindowManager.selfInst = None
 
 #-----------------------------------------------
+# start
+#-----------------------------------------------
 def start():
 	WindowManager.getManager().startLoop()
 
+#-----------------------------------------------
+# stop
 #-----------------------------------------------
 def stop():
 	WindowManager.killManager()
